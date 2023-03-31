1d254d31c3fab646741973a8f5e73f8c0e0d3d12
LDH <luvya8055@naver.com>
2023-03-31 11:48:54 +0900

    할 일 목록 check 구현

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import Todolist from './Todolist';
 
 const Todo = () => {
-  const [todos, setTodos] = useState([]); // 할 일 목록을 배열로 state에 저장
+  const [todos, setTodos] = useState([]); // 할 일 목록
 
   const inputValue = useRef(); // input에 입력한 값을 저장
 
@@ -11,15 +11,33 @@ const Todo = () => {
     const trimmedValue = inputValue.current.value.trim(); // 띄어쓰기 방지 trim으로 문자열의 앞 뒤 공백제거
     if (trimmedValue !== '') {
       // 입력값이 빈 문자열이 아닐 경우에만 동작
-      setTodos([...todos, trimmedValue]);
+      const newTodo = { id: Date.now(), content: trimmedValue, done: false };
+      setTodos([newTodo, ...todos]);
+      console.log(todos);
       inputValue.current.value = '';
     }
   };
 
   const TodoWrap = styled.div`
+    display: flex;
+    flex-direction: column;
     width: 400px;
     height: 600px;
     background-color: yellow;
+
+    & section {
+      flex: 1;
+      margin: 0px 10px 10px;
+      overflow: scroll;
+
+      /* 스크롤 숨기기 */
+      -ms-overflow-style: none; /* 인터넷 익스플로러 */
+      scrollbar-width: none; /* 파이어폭스 */
+
+      &::-webkit-scrollbar {
+        display: none; /* 크롬, 사파리, 오페라, 엣지 */
+      }
+    }
   `;
 
   const Header = styled.header`
@@ -35,15 +53,34 @@ const Todo = () => {
     }
   `;
 
+  const Input = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin: 10px;
+
+    & input {
+    }
+
+    & button {
+    }
+  `;
+
   return (
     <TodoWrap>
+      <button onClick={() => console.log(todos)}>TEST</button>
       <Header>
         <h1>Todo list</h1>
         <p>2023.03.30</p>
       </Header>
-      <input type='text' ref={inputValue} />
-      <button onClick={handleAddTodo}>Add Todo</button>
+      <Input>
+        <input type='text' ref={inputValue} />
+        <button onClick={handleAddTodo}>Add Todo</button>
+      </Input>
       <Todolist todos={todos} setTodos={setTodos} />
+      <Input>
+        <button>Clear checked</button>
+        <button>Clear All</button>
+      </Input>
     </TodoWrap>
   );
 };
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,13 +1,31 @@
 import styled from 'styled-components';
 const Todolist = ({ todos, setTodos }) => {
   const List = styled.ul``;
-  const ListItem = styled.li``;
+  const ListItem = styled.li`
+    display: flex;
+    justify-content: space-between;
+  `;
+
+  const deleteHandler = (id) => {
+    const newTodo = todos.filter((it) => it.id !== id); // 삭제 할 Todo와 ID가 같은 Todo를 제거한 새로운 배열을 만듬
+    setTodos(newTodo);
+  };
+
+  const checkDone = (it) => {
+    it.done = !it.done; // check box가 동작할때 마다 할일의 done 값을 바꿈
+  };
 
   return (
     <section>
       <List>
         {todos.map((it) => {
-          return <ListItem>{it}</ListItem>;
+          return (
+            <ListItem>
+              <input type='checkbox' name='check for doon' id={Date.now} onChange={() => checkDone(it)} />
+              <p>{it.content}</p>
+              <button onClick={() => deleteHandler(it.id)}>DELETE</button>
+            </ListItem>
+          );
         })}
       </List>
     </section>
